a68dcc930f98b672b10572b997408f124fa6a80b
TeddyGavi <matc.davis@gmail.com>
2023-06-04 23:26:46 -0700

    init

---
+++ lua/main/keymaps.lua
@@ -0,0 +1,15 @@
+-- Navigate vim panes better
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' ' 
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.shiftround = true 
+vim.opt.expandtab = true
+
+vim.opt.showcmd = true
+vim.opt.laststatus = 2
+vim.opt.autowrite = true
+vim.opt.autoread = true
+
+-- vim.keymap.set("n", "<leader>h", :nohlsearch<CR>)
+
