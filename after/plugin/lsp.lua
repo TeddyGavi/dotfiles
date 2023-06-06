5bea54d5bb71540829dc24bd70fe06e4c23293f6
TeddyGavi <matc.davis@gmail.com>
2023-06-06 13:48:46 -0700

    set up cmp autocomp

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -9,3 +9,22 @@ lsp.ensure_installed({
 lsp.nvim_workspace()
 
 lsp.setup()
+
+-- You need to setup `cmp` after lsp-zero
+-- default as per docs!
+local cmp = require('cmp')
+local cmp_action = require('lsp-zero').cmp_action()
+
+cmp.setup({
+  mapping = {
+    -- `Enter` key to confirm completion
+    ['<CR>'] = cmp.mapping.confirm({select = false}),
+
+    -- Ctrl+Space to trigger completion menu
+    ['<C-Space>'] = cmp.mapping.complete(),
+
+    -- Navigate between snippet placeholder
+    ['<C-f>'] = cmp_action.luasnip_jump_forward(),
+    ['<C-b>'] = cmp_action.luasnip_jump_backward(),
+  }
+})
