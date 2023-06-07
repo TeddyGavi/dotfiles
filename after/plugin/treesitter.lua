a42da398aca6ba2cfd7272d44d53d7563eebefa7
TeddyGavi <matc.davis@gmail.com>
2023-06-07 15:49:02 -0700

    add markdown

--- after/plugin/treesitter.lua
+++ after/plugin/treesitter.lua
@@ -1,6 +1,6 @@
 require'nvim-treesitter.configs'.setup {
   -- A list of parser names, or "all"
-  ensure_installed = { "vim", "query", "vimdoc", "javascript", "typescript", "c", "lua", "ruby" },
+  ensure_installed = { "vim", "query", "vimdoc", "javascript", "typescript", "c", "lua", "ruby", "markdown" },
 
   -- Install parsers synchronously (only applied to `ensure_installed`)
   sync_install = false,
